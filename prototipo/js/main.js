bbfc65ec63a63cd7e74dfb2da2abb1ed7965602d
pedroh1607 <107113002+pedroh1607@users.noreply.github.com>
2023-09-19 14:50:33 -0300

    Add files via upload

---
+++ prototipo/js/main.js
@@ -0,0 +1,47 @@
+function convertToJSON() {
+    const input = document.getElementsByName("xlsx-file") [0];
+    const file = input.files[0];
+  
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        const data = new Uint8Array(event.target.result);
+        const workbook = XLSX.read(data, { type: "array" });
+  
+        const jsonArrays = [];
+  
+        // Itera sobre cada planilha no arquivo
+        workbook.SheetNames.forEach((sheetName) => {
+          const worksheet = workbook.Sheets[sheetName];
+          const jsonData = XLSX.utils.sheet_to_json(worksheet, { raw: false });
+  
+          // Adicionando a detecção de coluna
+          const jsonWithColumnDetection = jsonData.map((row) => {
+            const rowWithColumnDetection = {};
+            for (const column in row) {
+              const columnIndex = XLSX.utils.decode_col(column);
+              rowWithColumnDetection[column] = {
+                value: row[column],
+                columnIndex: columnIndex
+              };
+            }
+            return rowWithColumnDetection;
+          });
+  
+          jsonArrays.push(jsonWithColumnDetection);
+        });
+  
+        fetch("https://4sqwxk-9128.csb.app/horario/edit", {
+          method: "POST",
+          body: JSON.stringify(jsonArrays),
+          headers: { "Content-Type": "application/json" }
+        })
+          .then((res) => res.json())
+          .then((data) => console.log(data))
+          .catch((err) => console.log(err));
+      };
+  
+      reader.readAsArrayBuffer(file);
+    }
+  }
+  
